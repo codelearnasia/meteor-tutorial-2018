bdfa767b7a7f547820767cfcb1a32654937b4965
Zak Burki <zakburki@Zaks-MacBook-Pro.local>
2018-06-17 21:36:38 +0800

    meteor tutorial part 7

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,13 +1,26 @@
 import { Template } from 'meteor/templating';
 import { Tasks } from '../api/tasks.js';
+import { ReactiveDict } from 'meteor/reactive-dict';
 
 import './task.js';
 import './body.html';
 
+Template.body.onCreated(function bodyOnCreated() {
+  this.state = new ReactiveDict();
+});
+
 Template.body.helpers({
   tasks() {
-    // Show newest tasks at the top
+    const instance = Template.instance();
+    if (instance.state.get('hideCompleted')) {
+      // If hide completed is checked, filter tasks
+      return Tasks.find({ checked: { $ne: true } }, { sort: { createdAt: -1 } });
+    }
+    // Otherwise, return all of the tasks
     return Tasks.find({}, { sort: { createdAt: -1 } });
+  },
+  incompleteCount() {
+    return Tasks.find({ checked: { $ne: true } }).count();
   },
 });
 
@@ -23,7 +36,6 @@ Template.body.events({
     const text = target.text.value;
 
     // Insert a task into the collection
-
     Tasks.insert({
       text,
       createdAt: new Date(), // current time
@@ -31,7 +43,10 @@ Template.body.events({
 
     // Clear form
     target.text.value = '';
+  },
 
+  'change .hide-completed input'(event, instance) {
+    instance.state.set('hideCompleted', event.target.checked);
   },
 
 });
